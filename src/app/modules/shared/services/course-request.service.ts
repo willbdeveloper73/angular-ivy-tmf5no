66bfaa230b4bc209d6d4b88448e3bbc790aa6576
Mark Peters <mpskeeter@gmail.com>
2022-03-14 13:54:32 -0400

    Update course-request.service.ts

--- src/app/modules/shared/services/course-request.service.ts
+++ src/app/modules/shared/services/course-request.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CrudService } from './crud.service';
-import { CourseRequest } from '../models';
+import { CourseRequest } from '../../shared-types';
 
 @Injectable({ providedIn: 'root' })
 export class CourseRequestService extends CrudService<CourseRequest> {
