7ffb61c5347069c8d272b43ffa61b4967d2205ec
Mark Peters <mpskeeter@gmail.com>
2022-03-14 14:52:17 -0400

    Update index.ts

--- src/app/modules/shared/forms/index.ts
+++ src/app/modules/shared/forms/index.ts
@@ -2,6 +2,7 @@ export * from './category-form.service';
 export * from './course-form.service';
 export * from './course-request-form.service';
 export * from './maintenance-log-form.service';
+export * from './play-list-form.service';
 export * from './play-list-item-form.service';
 export * from './requestor-info-form.service';
 export * from './role-form.service';
