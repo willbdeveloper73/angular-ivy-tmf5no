93ad88543551273bc0874a659da99b73fca8dddf
Mark Peters <mpskeeter@gmail.com>
2022-03-14 11:36:07 -0400

    Update course-form.service.ts

--- src/app/modules/shared/forms/course-form.service.ts
+++ src/app/modules/shared/forms/course-form.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { BaseForm } from './base-form.service';
-import { Course } from '../models';
+import { Course } from '../../shared-types';
 
 import { convertDate } from './utils';
 
