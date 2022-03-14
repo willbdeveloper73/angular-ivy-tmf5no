4a74b89d182ffd00f703c20a769e6d2728710181
Mark Peters <mpskeeter@gmail.com>
2022-03-14 11:57:09 -0400

    Delete category.interface.ts

--- src/app/modules/shared/models/category.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Course } from './course.interface';
-import { BaseName } from './base-name.interface';
-
-export interface Category extends BaseName {
-  description?: string;
-  image?: string;
-
-  courses?: Partial<Course>[];
-}
