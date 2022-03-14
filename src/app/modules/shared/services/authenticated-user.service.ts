2ac45072dec074b076b933d58dda8d7b53fca784
Mark Peters <mpskeeter@gmail.com>
2022-03-14 12:06:13 -0400

    Update authenticated-user.service.ts

--- src/app/modules/shared/services/authenticated-user.service.ts
+++ src/app/modules/shared/services/authenticated-user.service.ts
@@ -1,18 +1,14 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
-import { Enrollment, Role, User, Watched } from '../models';
+import { Enrollment } from '../../shared-types';
+import { Role, User, Watched } from '../models';
 import { CrudService } from './crud.service';
 import { CourseService } from './course.service';
 import { UserService } from './user.service';
 import { EnrollmentService } from './enrollment.service';
 import { WatchedService } from './watched.service';
 import { rawUsers } from './rawData';
-
-// interface Watched {
-//   courseId: number;
-//   itemId: number;
-// }
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticatedUserService extends CrudService<User> {
