a82232d653bc26e9bae2c1ee665f38bc3f280116
Mark Peters <mpskeeter@gmail.com>
2022-03-14 12:37:37 -0400

    Update play-list-item.interface.ts

--- src/app/modules/shared-types/models/play-list-item.interface.ts
+++ src/app/modules/shared-types/models/play-list-item.interface.ts
@@ -1,11 +1,8 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
-
 import { BaseName } from './base-name.interface';
 import { FormTableElement } from './form-table-element.interface';
 import { PlayListSource } from './play-list-source.interface';
 import { User } from './user.interface';
 import { Status } from './status.interface';
-import { convertDate } from './utils';
 
 export interface PlayListItem extends BaseName {
   seq?: number;
@@ -21,43 +18,6 @@ export interface PlayListItem extends BaseName {
   status?: Partial<Status>[];
   author?: Partial<User>;
 }
-
-export const generatePlayListItemForm = (
-  // fb: FormBuilder,
-  record: Partial<PlayListItem> = {}
-): FormGroup => {
-  const fb = new FormBuilder();
-  return fb.group({
-    id: [record?.id],
-    name: [record?.name],
-    seq: [record?.seq],
-    description: [record?.description],
-    statusId: [record?.statusId],
-    createdAt: [convertDate(record?.createdAt)],
-    updatedAt: [convertDate(record?.updatedAt)],
-    deletedAt: [convertDate(record?.deletedAt)],
-    authorId: [record?.authorId],
-    duration: [record?.duration],
-    watched: [record?.watched],
-  });
-};
-
-export const generatePlayListItemFromForm = (form: FormGroup): PlayListItem => {
-  const record: Partial<PlayListItem> = {
-    id: form.get('id').value,
-    name: form.get('name').value,
-    seq: parseInt(form.get('seq').value, 10),
-    description: form.get('description').value,
-    statusId: parseInt(form.get('statusId').value, 10),
-    createdAt: form.get('createdAt').value,
-    updatedAt: form.get('updatedAt').value,
-    deletedAt: form.get('deletedAt').value,
-    authorId: parseInt(form.get('authorId').value, 10),
-    duration: form.get('duration').value,
-    watched: form.get('watched').value,
-  };
-  return record;
-};
 
 export const PlayListItemElements: Partial<FormTableElement>[] = [
   {
