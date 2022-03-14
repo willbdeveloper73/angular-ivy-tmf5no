58b20bc8a31a6dbe611e2f1313f5ffa1eb841fc1
Mark Peters <mpskeeter@gmail.com>
2022-03-14 14:04:29 -0400

    Update index.ts

--- src/app/modules/shared-types/models/index.ts
+++ src/app/modules/shared-types/models/index.ts
@@ -2,6 +2,7 @@ export * from './base-id.interface';
 export * from './base-name.interface';
 export * from './category.interface';
 export * from './course.interface';
+export * from './course-list-meta.interface';
 export * from './course-request.interface';
 export * from './enrollment.interface';
 export * from './form-table-element.interface';
