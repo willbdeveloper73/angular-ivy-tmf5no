b75b58afc30b6f3bd168ca718b9b87c6adb05bb1
Mark Peters <mpskeeter@gmail.com>
2022-03-14 13:21:54 -0400

    Update index.ts

--- src/app/modules/shared-types/models/index.ts
+++ src/app/modules/shared-types/models/index.ts
@@ -10,7 +10,10 @@ export * from './play-list-item.interface';
 export * from './play-list-source.interface';
 // export * from './role.interface';
 export * from './select-item.interface';
+export * from './settings.interface';
 export * from './status.interface';
 export * from './tag.interface';
-// export * from './user.interface';
+export * from './user.interface';
+export * from './watched.interface';
+
 export * from './utils';
