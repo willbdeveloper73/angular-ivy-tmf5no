8bbbd84449cf47820703ec85f7ce72204152639c
Mark Peters <mpskeeter@gmail.com>
2022-03-14 09:04:06 -0400

    Update course.service.ts

--- src/app/modules/shared/services/course.service.ts
+++ src/app/modules/shared/services/course.service.ts
@@ -3,7 +3,7 @@ import { combineLatest, Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { CrudService } from './crud.service';
 import { rawCourses } from './rawData';
-import { Course, CourseCategory, CourseListMeta, Tag } from '../models';
+import { Course, Category, CourseListMeta, Tag } from '../models';
 
 @Injectable({ providedIn: 'root' })
 export class CourseService extends CrudService<Course> {
@@ -46,7 +46,7 @@ export class CourseService extends CrudService<Course> {
   getAllForCategory(categoryId: number) {
     const items = this._items.filter((item) =>
       item.categories.some(
-        (category: Partial<CourseCategory>) => category.id === categoryId
+        (category: Partial<Category>) => category.id === categoryId
       )
     );
     this.items.next(items.sort(this.asc));
